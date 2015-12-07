9fd48f65946f9d871ce8711ad41cea6c0a979d72
Emily Dolson <EmilyLDolson@gmail.com>
2015-12-07 14:01:24 -0500

    Added toroidal spatial weights function

---
+++ avidaspatial/make_distance_matrix.py
@@ -0,0 +1,48 @@
+import pysal
+
+x = 5
+y = 5
+
+w = pysal.lat2W(x, y).neighbors
+
+for i in range(x*y):
+
+    if i == 0:
+        #Upper left
+        w[i].append(x-1)
+        w[i].append(x*(y-1))
+
+    elif i == x-1:
+        #Upper right
+        w[i].append(0)
+        w[i].append((x*y)-1)
+
+    elif i == x*(y-1):
+        #Lower left
+        w[i].append((x*y)-1)
+        w[i].append(0)
+
+    elif i == (x*y)-1:
+        print "lower right"
+        #Lower right
+        w[i].append(((x*y)-1))
+        w[i].append(x-1)
+
+    elif i % x == 0:
+        #we are on the left edge
+        w[i].append(i+x-1)
+        
+    elif i%x == x-1:
+        #we are on the right edge
+        w[i].append(i-x+1)
+            
+    elif i // x == 0:
+        #we are on the top edge
+        w[i].append((x*(y-1)) + i % x)
+
+    elif i // x == y-1:
+        w[i].append(i%x)
+  
+
+weights = pysal.weights.weights.W(w)
+print weights.full()
